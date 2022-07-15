4cfa86117f7034a01932064eae58904e6c036c0d
DESKTOP-I01CR6B\fndsa <fndsanto@gmail.com>
2022-07-15 17:25:12 +0700

    add validation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <iterator>
+#include <vector>
 using namespace std;
 
 class Menu {
@@ -45,7 +46,6 @@ class Order {
     int getQuantity() {
         return quantity;
     }
-
     Order(string menu, int quantity) {
         this->menu = menu;
         this->quantity = quantity;
@@ -59,40 +59,10 @@ void printFoods() {
     foods.insert(make_pair(3, new Menu("Fried Chicken", 15000)));
     foods.insert(make_pair(4, new Menu("Rendang Beef", 55000)));
     foods.insert(make_pair(5, new Menu("Fish n Chips", 65000)));
-    cout << "no" << "\t" << "   name" << "\t\t" << "price" << endl;
+    cout << "no" << "\t" << "   name" << "\t\t\t   " << "price" << endl;
     for (auto it = foods.begin(); it != foods.end(); ++it) {
-      cout << it->first << "\t" << it->second->getName() << "\t" << it->second->getPrice() << endl;
+      cout << it->first << "\t" << it->second->getName() << "\t\t Rp." << it->second->getPrice() << endl;
     }
-}
-
-void display() {
-    char input1, input2, input3;
-    int orderNum;
-    do {
-        cout << "-----------------------------" << endl;
-        cout << "Welcome to Amazing Restaurant" << endl;
-        cout << "-----------------------------" << endl << endl;
-        do {
-            cout << "Do you want to order Food?" << endl;
-            cin >> input1;
-        } while (input1 != 'n' && input1 != 'y');
-        cout << "Please choose our Food by selecting number below:" << endl;
-        printFoods();
-        cout << endl;
-        do {
-            cout << "Your Food order number?" << endl;
-            cin >> orderNum;
-        } while (orderNum <= 0 || orderNum > 6);
-        do {
-            cout << "Do you want to order Drink?" << endl;
-            cin >> input2;
-        } while (input2 != 'n');
-        do {
-            cout << "Do you want to order more?" << endl;
-            cin >> input3;
-        } while (input3 != 'n');
-    } while (input3 != 'n');
-
 }
 
 void printDrinks() {
@@ -102,17 +72,106 @@ void printDrinks() {
     drinks.insert(make_pair(3, new Menu("Fruit Tea", 3500)));
     drinks.insert(make_pair(4, new Menu("Coffee\t", 7000)));
     drinks.insert(make_pair(5, new Menu("Water\t", 2000)));
-    cout << "no" << "\t" << "   name" << "\t\t" << "price" << endl;
+    cout << "no" << "\t" << "   name" << "\t\t\t   " << "price" << endl;
     for (auto it = drinks.begin(); it != drinks.end(); ++it) {
-      cout << it->first << "\t" << it->second->getName() << "\t" << it->second->getPrice() << endl;
+      cout << it->first << "\t" << it->second->getName() << "\t\t Rp." << it->second->getPrice() << endl;
     }
 }
 
+void display() {
+    char input;
+    int orderNum, quantity;
+    vector<Order*> orders;
+    cout << "-----------------------------" << endl;
+    cout << "Welcome to Amazing Restaurant" << endl;
+    cout << "-----------------------------" << endl << endl;
+    do {
+        cout << "Do you want to order Food?" << endl;
+        cin >> input;
+
+        if(input=='y') {
+            do {
+                cout << "Please choose our Food by selecting number below:" << endl;
+                printFoods();
+                cout << endl;
+                do {
+                    cout << "Your Food order number?" << endl;
+                    cin >> orderNum;
+                    if(cin.fail()){
+                        cin.clear(cin.rdstate() & ~ios_base::failbit);
+                        cin.ignore();
+                    }
+                } while (orderNum <= 0 || orderNum > 6 && !cin.fail());
+                cout << "How many quantity?" << endl;
+                do {
+                    cin >> quantity;
+                    if(cin.fail()){
+                        cout << "You entered an incorrect value. Try again" << endl;
+                        cin.clear(cin.rdstate() & ~ios_base::failbit);
+                        cin.ignore();
+                    }
+                } while (quantity <= 0 && !cin.fail());
+                
+                Order *order = new Order;
+                order->setMenu = "sa";
+                order->setQuantity = quantity;
+                orders.push_back(*order);
+                for(vector<Order>::iterator itr=orders.begin();itr!=orders.end();++itr)  
+                    cout<<*itr << endl;
+                do {
+                    cout << "Do you want to order more?" << endl;
+                    cin >> input;
+                } while (input != 'y' && input != 'n');
+            } while (input != 'n');
+        }
+    } while (input != 'n' && input != 'y');
+
+    do {
+        cout << "Do you want to order Drink?" << endl;
+        cin >> input;
+
+        if (input == 'y') {
+            do {
+                cout << "Please choose our Food by selecting number below:" << endl;
+                printDrinks();
+                cout << endl;
+                do {
+                    cout << "Your Drink order number?" << endl;
+                    cin >> orderNum;
+                } while (orderNum <= 0 || orderNum > 6 && !cin.fail());
+                cout << "How many quantity?" << endl;
+                do {
+                    cin >> quantity;
+                    if(cin.fail()){
+                        cout << "You entered an incorrect value. Try again" << endl;
+                        cin.clear(cin.rdstate() & ~ios_base::failbit);
+                        cin.ignore();
+                    }
+                } while (quantity <= 0 && !cin.fail());
+                // Order *order = new Order;
+                // order->setMenu = "sa";
+                // order->setQuantity = quantity;
+                // orders.push_back(*order);
+                // for(vector<Order>::iterator itr=orders.begin();itr!=orders.end();++itr)  
+                //     cout<<*itr << endl;
+                do {
+                    cout << "Do you want to order more?";
+                    cin >> input;
+                } while (input != 'y' && input != 'n');
+            } while (input != 'n');
+        }
+    } while (input != 'n' && input != 'y');
+}
+
 int main() {
-    display();
+    char input;
+    do {
+        display();
+        do {
+            cout << "Do you want to order more?" << endl;
+            cin >> input;
+        } while (input != 'n' && input != 'y');
+    } while (input != 'n');
     cout << "Thank you for coming!";
-    // printDrinks();
-    // Order order;
-    // Menu("Ayam", 12000);
     return 0;
 }
